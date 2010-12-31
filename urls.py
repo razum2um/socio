3c5e129f5770241c738d5780be9ca994e18e4d14
Egor Nazarkin <nimnull@gmail.com>
2010-12-31 18:47:02 +0600

    Updated styles

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -13,14 +13,13 @@ urlpatterns = patterns('',
     (r'^profiles/', include('profiles.urls')),
     (r'^comments/', include('comments.urls')),
     (r'^', include('blogs.urls')),
-    # Example:
-    # (r'^socio/', include('socio.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
 
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^media/(?P<path>.*)$',
+            'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}
+        ),
+    )
