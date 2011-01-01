441f9665cf3a79bbea6dff1558c66e2e47734273
Egor Nazarkin <nimnull@gmail.com>
2011-01-02 02:54:45 +0600

    Created photo models migrations

--- apps/profiles/models/__init__.py
+++ apps/profiles/models/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from profilemodel import UserProfile
 from attributemodel import ProfileAttribute
+from photomodel import PhotoAlbum, Photo
 from helpers import SEX_CHOICES, S_FEMALE, S_MALE, S_UNDEF
 
 
